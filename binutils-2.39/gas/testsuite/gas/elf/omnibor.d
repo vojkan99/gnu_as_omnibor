c768c8d136cc6d8732f90be70f9bcaed84ab0e2d
Vojislav Tomasevic <vojkan99@gmail.com>
2024-12-29 22:46:21 +0100

    Adding tests for the OmniBOR concept in as tool
    Signed-off-by: Vojislav Tomasevic <vojkan99@gmail.com>

---
+++ binutils-2.39/gas/testsuite/gas/elf/omnibor.d
@@ -0,0 +1,11 @@
+#source: omnibor.s
+#readelf: -x .note.omnibor
+
+Hex dump of section '.note.omnibor':
+  [x0-9]+ 08000000 14000000 01000000 4f4d4e49 ............OMNI
+  [x0-9]+ 424f5200 fe2bd8f7 4815e578 a2eed695 BOR..\+..H..x....
+  [x0-9]+ 357239e5 4fe88848 08000000 20000000 5r9.O..H.... ...
+  [x0-9]+ 02000000 4f4d4e49 424f5200 58fc9822 ....OMNIBOR.X.."
+  [x0-9]+ 59aecdb0 7634390a 8b4ba1ac 3024c40a Y...v49..K..0\$..
+  [x0-9]+ c1f2770a 17451c87 cf2e313c          ..w..E....1<
+
